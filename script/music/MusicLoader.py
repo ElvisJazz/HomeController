b63687b53520848d8f2875c81051e5aa66f1b839
ElvisJazz <285835174@qq.com>
2017-06-25 22:00:18 +0800

    fix load files error

--- script/music/MusicLoader.py
+++ script/music/MusicLoader.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from genericpath import isdir
 import os
 from os.path import isfile, join
 
@@ -10,9 +11,9 @@ class MusicLoader(object):
 	@staticmethod
 	def load_music_lists(music_path):
 		music_list_dir = os.listdir(music_path)
-		return [MusicLoader.list_all_files(music_list) for music_list in music_list_dir]
+		return [MusicLoader.list_all_files(music_list) for music_list in music_list_dir if isdir(music_list)]
 
 	@staticmethod
 	def list_all_files(path):
-		return [join(path, file) for file in path if isfile(join(path, file))]
+		return [join(path, file) for file in os.listdir(path) if isfile(join(path, file))]
 
